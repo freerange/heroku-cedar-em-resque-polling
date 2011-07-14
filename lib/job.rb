57be290eebdcafd953f7b4b2688bedaf0a5081d2
James Mead <james@floehopper.org>
2011-07-14 13:37:46 +0100

    Need to require Resque in job file.

--- lib/job.rb
+++ lib/job.rb
@@ -1,3 +1,4 @@
+require "resque"
 ENV["REDISTOGO_URL"] ||= "redis://redistogo:e0118abf0de6069ff50f980889ae451e@catfish.redistogo.com:9456/"
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
