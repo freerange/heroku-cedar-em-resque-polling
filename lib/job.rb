7306789abee2cab9554f3377bab695a4908cd300
James Mead <james@floehopper.org>
2011-07-14 13:30:10 +0100

    Setup Resque with RedisToGo add-on details.

--- lib/job.rb
+++ lib/job.rb
@@ -1,3 +1,7 @@
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:e0118abf0de6069ff50f980889ae451e@catfish.redistogo.com:9456/"
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 class Job
   class << self
     def queue
