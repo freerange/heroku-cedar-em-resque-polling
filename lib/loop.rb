d9aacb7cbb54a00f866a8034a72dcb157dc060dc
James Mead <james@floehopper.org>
2011-07-14 15:21:03 +0100

    Add some logging.

--- lib/loop.rb
+++ lib/loop.rb
@@ -7,7 +7,11 @@ puts "Starting worker for queue: #{queue}; and with polling interval: #{interval
 EventMachine::run {
   EventMachine::add_periodic_timer(interval) do
     if job = Resque::Job.reserve(queue)
+      puts "Performing job..."
       job.perform
+      puts "...done."
+    else
+      puts "No job found."
     end
   end
 }
